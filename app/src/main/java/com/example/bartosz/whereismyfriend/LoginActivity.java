2b7722b85f1d95cf2df4e7e6bd8188492b5b05ac
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2017-08-28 21:45:37 +0200

    Fix login activity

--- app/src/main/java/com/example/bartosz/whereismyfriend/LoginActivity.java
+++ app/src/main/java/com/example/bartosz/whereismyfriend/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.bartosz.whereismyfriend;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,6 +18,9 @@ import com.example.bartosz.whereismyfriend.Models.User;
 import com.example.bartosz.whereismyfriend.Models.Location;
 import com.example.bartosz.whereismyfriend.Models.Invitation;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,30 +32,31 @@ import com.google.firebase.database.ValueEventListener;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseDatabase _database;
-    //private FirebaseAuth _firebaseAuth;
     private DatabaseReference _databaseRef;
+    private FirebaseAuth _firebaseAuth;
+    private FirebaseAuth.AuthStateListener _authStateListener;
 
-
-    //private ProgressDialog progressBar;
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button buttonLogin;
+    private TextView tvRegisterLink;
+    private ProgressDialog progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //progressBar = new ProgressDialog(this);
-        final EditText etEmail = (EditText) findViewById(R.id.loginEmail);
-        final EditText etPassword = (EditText) findViewById(R.id.loginPassword);
+        etEmail = (EditText) findViewById(R.id.loginEmail);
+        etPassword = (EditText) findViewById(R.id.loginPassword);
 
-        final Button buttonLogin = (Button) findViewById(R.id.loginButton);
-        final TextView tvRegisterLink = (TextView) findViewById(R.id.loginRegisterHere);
+        buttonLogin = (Button) findViewById(R.id.loginButton);
+        tvRegisterLink = (TextView) findViewById(R.id.loginRegisterHere);
 
 
         _database = FirebaseDatabase.getInstance();
         _databaseRef = _database.getReference();
-        //_firebaseAuth = FirebaseAuth.getInstance();
-
-
+        _firebaseAuth = FirebaseAuth.getInstance();
 
         tvRegisterLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,40 +66,45 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        buttonLogin.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Location location = new Location(52.390758, 16.951566);
-                Invitation invitation = null;
-                User user = new User();
-                user.UserName = "mkaczorowski";
-                user.Name = "Miłosz";
-                user.Surname = "Kaczorowski";
-                user.Age = 34;
-                user.Email = "mkaczorowski@whereismyfriend.com";
-                user.SendInvitations = null;
-                user.RecivedInvitations = null;
-                user.Range = 3000.0;
-
-                //DatabaseReference myData = _databaseRef.child("Users").child("mkaczorowski").getRef();//.setValue(user);
-
-                _databaseRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        Log.d("onDataChange", dataSnapshot.toString());
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                LoginUser();
+                Intent registerIntent = new Intent(LoginActivity.this, Home.class);
+                LoginActivity.this.startActivity(registerIntent);
             }
-        });
+        }));
+
+        _authStateListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(_firebaseAuth.getCurrentUser() != null){
+                    String email = _firebaseAuth.getCurrentUser().getEmail();
+                    if(email != null)
+                    {
+                        Intent loginIntent = new Intent(LoginActivity.this, Home.class);
+                        startActivity(loginIntent);
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        _firebaseAuth.addAuthStateListener(_authStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        _firebaseAuth.removeAuthStateListener(_authStateListener);
     }
 
     private void LoginUser(){
-        /*String email = etEmail.getText().toString().trim();
+
+        String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
         if(TextUtils.isEmpty(email))
@@ -109,7 +119,19 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         progressBar.setMessage("Loging user...");
-        progressBar.show();*/
-        //_firebaseAuth.
+        progressBar.show();
+
+        _firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed. Please try again", Toast.LENGTH_SHORT).show();
+                        }
+                        progressBar.hide();
+                    }
+                });
     }
 }
